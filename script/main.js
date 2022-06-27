70cae6efe194240ddc43aa3ba49ecc37b97b1fb9
Ygor Goulart <ygorgm95@gmail.com>
2022-06-27 01:01:45 -0300

    index responsiveness

---
+++ script/main.js
@@ -0,0 +1,7 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+
+
+function responsiveMenu() {
+    var x = document.getElementById("nav-links");
+    x.classList.toggle('active');
+  }
