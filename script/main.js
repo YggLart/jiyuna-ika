cdd6fff756c35ea2445a20d6e3c4476b781d4799
Ygor Goulart <ygorgm95@gmail.com>
2022-06-30 08:18:13 -0300

    base fotos

--- script/main.js
+++ script/main.js
@@ -1,25 +1,9 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-
-
 function responsiveMenu() {
     var x = document.getElementById("nav-links");
     x.classList.toggle('active');
   }
 
+  function fotoView(e) {
+    e.classList.toggle('active');
+  }
 
-
-  // Include Lightbox 
-import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
-
-const lightbox = new PhotoSwipeLightbox({
-  // may select multiple "galleries"
-  gallery: '#gallery--getting-started',
-
-  // Elements within gallery (slides)
-  children: 'a',
-
-  // setup PhotoSwipe Core dynamic import
-  pswpModule: () => import('/photoswipe/photoswipe.esm.js')
-});
-lightbox.init();
-  
